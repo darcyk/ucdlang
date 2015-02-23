7ff6f1430417cca51b21224ddc03c86757ce3dd4
Kev D'Arcy <kev.darcy@gmail.com>
2015-02-23 20:28:13 +0000

    Initial test seeding

---
+++ neo4j-interface/src/main/java/test/Email.java
@@ -0,0 +1,5 @@
+package test;
+
+
+public interface Email extends Message {
+}
---
+++ neo4j-interface/src/main/java/test/GraphManager.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.List;
+
+public interface GraphManager {
+	
+	public void addPerson(Person person) throws IllegalArgumentException;
+	
+	public void linkPerson(Person person, Person knows) throws IllegalArgumentException;
+	
+	public void addAccount(Account account) throws IllegalArgumentException;
+	
+	public void linkAccount(Person person, Account account) throws IllegalArgumentException;
+	
+	public void addMessage(Message msg, Account from, List<Account> to) throws IllegalArgumentException;
+}
