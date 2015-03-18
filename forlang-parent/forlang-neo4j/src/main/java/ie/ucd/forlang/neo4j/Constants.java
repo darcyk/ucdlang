1f03552a85836acff67f3f6d236d146615abea80
Kev D'Arcy <kev.darcy@gmail.com>
2015-03-18 23:26:29 +0000

    Add twitter account object

--- forlang-parent/forlang-neo4j/src/main/java/ie/ucd/forlang/neo4j/Constants.java
+++ forlang-parent/forlang-neo4j/src/main/java/ie/ucd/forlang/neo4j/Constants.java
@@ -11,6 +11,14 @@ public final class Constants {
 	public static final String PROP_RECIPIENTS = "recipients";
 	public static final String PROP_SENDER = "sender";
 	public static final String PROP_SUBJECT = "subject";
+	public static final String PROP_TWITTER_CREATED_AT = "createdAt";
+	public static final String PROP_TWITTER_DESCRIPTION = "description";
+	public static final String PROP_TWITTER_FOLLOWERS_COUNT = "followersCount";
+	public static final String PROP_TWITTER_FRIENDS_COUNT = "friendsCount";
+	public static final String PROP_TWITTER_GEO_ENABLED = "geoEnabled";
+	public static final String PROP_TWITTER_ID = "twitterId";
+	public static final String PROP_TWITTER_LOCATION = "location";
+	public static final String PROP_TWITTER_SCREEN_NAME = "screenName";
 
 	// make sure you can't instantiate
 	private Constants() {
